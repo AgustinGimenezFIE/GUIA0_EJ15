ce69f86f33a311e9e212b5248d64c9b525edbfcd
Agustin <darkstone19@gmail.com>
2023-04-19 21:56:06 -0300

    Primer commit

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/*
+  EJERCICIO 15 : Defina una función que reciba dos cadenas de caracteres y calcule la unión entre ambas, devolviéndola en una nueva cadena de caracteres.
+ */
+
 int main() {
     printf("Hello, World!\n");
     return 0;
